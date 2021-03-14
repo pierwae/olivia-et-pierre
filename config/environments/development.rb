ccce276d2323bcfef7757aeb03d3d66d763ebab9
Pierre Waechter <pierre.waechter@audencia.com>
2021-03-14 13:27:22 +0100

    updating text

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports.
   config.consider_all_requests_local = false
