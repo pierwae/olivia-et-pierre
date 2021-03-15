2baa756a83b0bd2ff91b3c3bf966ad56de401867
Pierre Waechter <pierre.waechter@audencia.com>
2021-03-15 23:44:28 +0100

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,10 +5,9 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Do not eager load code on boot.
-
-  config.eager_load = true
+  config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = false
@@ -34,31 +33,9 @@ Rails.application.configure do
   config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
-  # config.action_mailer.raise_delivery_errors = true
-
-  # config.action_mailer.perform_caching = false
-
-
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.delivery_method = :smtp
-  host = 'www.oliviaetpierre.com' #replace with your own url
-  config.action_mailer.default_url_options = { host: host }
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: "smtp.gmail.com",
-    port: 587,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    openssl_verify_mode: "none",
-  }
-
-
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
